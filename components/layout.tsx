6e4fa7907c492255c80edbbf29750b211dc552b6
RRRGR <82594900+RRRGR@users.noreply.github.com>
2023-11-25 19:40:58 +0100

    delete meta

--- components/layout.tsx
+++ components/layout.tsx
@@ -6,12 +6,6 @@ import Head from "next/head";
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <>
-      <Head>
-        <meta
-          httpEquiv="Content-Security-Policy"
-          content="upgrade-insecure-requests"
-        />
-      </Head>
       <Header />
       <main>{children}</main>
       <Footer />
